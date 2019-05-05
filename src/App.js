738e940dee8e728bb16dc9a98a7ff5fbb57c563c
heliang0915 <598481337@qq.com>
2019-05-05 09:43:53 +0800

    efefef

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React123456
+          Learn React App
         </a>
       </header>
     </div>
