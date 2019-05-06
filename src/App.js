156817971e98c35017a0ce76b03f5d98a5eb295a
heliang0915 <598481337@qq.com>
2019-05-06 09:37:11 +0800

    修复

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React App1122323
+          Learn React App1122323dddd
         </a>
       </header>
     </div>
